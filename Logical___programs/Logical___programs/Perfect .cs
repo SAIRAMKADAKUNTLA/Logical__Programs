d155728c8dfa323caaa3f6092a08ddd72463d474
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 13:11:01 +0530

    Added:finding Perfect number

---
+++ Logical___programs/Logical___programs/Perfect .cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical__Programs
+{
+    class Perfect
+    {
+        
+            public void Check()
+            {
+                int Number, i, Sum = 0;
+                Console.WriteLine("Enter the number ");
+                Number = Convert.ToInt32(Console.ReadLine());
+                for (i = 1; i <= Number; i++)
+                {
+                    if (Number % i == 0)
+                        Sum += i;
+                }
+                if (Sum == Number)
+                    Console.Write(" It is a perfect number");
+                else
+                    Console.Write(" It is not a perfect number");
+            }
+        }
+    }
+
+
