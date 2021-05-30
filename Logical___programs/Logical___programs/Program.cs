f6deedb15586dd7707347120b0451798396a267f
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 13:14:47 +0530

    Added:Reversing the number

--- Logical___programs/Logical___programs/Program.cs
+++ Logical___programs/Logical___programs/Program.cs
@@ -7,6 +7,32 @@ namespace Logical___programs
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical problems");
+            Console.WriteLine("1.Fibonacci,2.Prime,3.Reverse,4.Perfect");
+            Console.WriteLine("Enter your Choice");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.Check();
+                    break;
+                case 2:
+                    Prime prime = new Prime();
+                    prime.Check();
+                    break;
+                case 3:
+                    Reverse reverse = new Reverse();
+                    reverse.check();
+                    break;
+                case 4:
+                    Perfect perfect = new Perfect();
+                    perfect.Check();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+
+            }
         }
     }
-}
+
+
