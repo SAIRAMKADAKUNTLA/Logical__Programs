c0d93ad3453d181289e3bdd75f7456c3c02cbf02
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 13:12:33 +0530

    Added:Reversing the number

---
+++ Logical___programs/Logical___programs/Reverse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical__Programs
+{
+    class Reverse
+    {
+        public void check()
+        {
+            int Reverse = 0, Number, Remainder;
+            Console.WriteLine("Enter the number ");
+            Number = Convert.ToInt32(Console.ReadLine());
+            while (Number != 0)
+            {
+                Remainder = Number % 10;
+                Reverse = Reverse * 10 + Remainder;
+                Number /= 10;
+            }
+            Console.Write("Reversed Number: " + Reverse);
+        }
+    }
+
+}
