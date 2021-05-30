5b94c4a476584b26a647d60faf82d39646aa4a4f
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 13:11:46 +0530

    Added:finding Prime number

---
+++ Logical___programs/Logical___programs/Prime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical__Programs
+{
+    class Prime
+    {
+        public void Check()
+        {
+            int Number, i, m;
+            Console.WriteLine("Enter the number ");
+            Number = Convert.ToInt32(Console.ReadLine());
+            m = Number / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (Number % i == 0)
+
+                    Console.WriteLine("Number is not a Prime");
+                else
+                    Console.WriteLine("Number is Prime");
+            }
+        }
+    }
+}
