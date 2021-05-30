4bc6ed36396ffa5dcc75ad4d30a32239a518c9b3
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-05-30 13:09:15 +0530

    Added:finding Fibonacci

---
+++ Logical___programs/Logical___programs/Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical__Programs
+{
+    class Fibonacci
+    {
+        public void Check()
+        {
+            int FirstN = 0, SecondN = 1, ThirdN, i, Number;
+            Console.WriteLine("Enter the number of Elements");
+            Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(FirstN + " " + " " + SecondN);
+            for (i = 2; i <= Number; i++)
+            {
+                ThirdN = FirstN + SecondN;
+                Console.WriteLine(ThirdN + " ");
+                FirstN = SecondN;
+                SecondN = ThirdN;
+            }
+        }
+    }
+}
